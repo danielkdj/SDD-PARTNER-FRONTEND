fd16cc973e08ef8b6b861b30d9e1b975098a8ff4
KYW456 <126450114+KYW456@users.noreply.github.com>
2023-05-09 16:07:29 +0900

    first commit

---
+++ src/views/ea/eaRequest.vue
@@ -0,0 +1,235 @@
+<template>
+    <div class="dashboard p-4">
+        <div class="mt-2 lg:flex block lg:gap-2 relative">
+            <div
+                    class="mt-2 bg-white dark:bg-gray-800 p-5 w-full rounded-md box-border border dark:border-gray-700"
+            >
+                <h2 class="font-bold text-lg text-gray-800 dark:text-gray-200">
+                    전자결재
+                </h2>
+                <div class="wrapping-table mt-10">
+                    <table
+                            class="w-full text-sm text-left text-gray-500 dark:text-gray-400 lg:overflow-auto overflow-x-scroll"
+                    >
+                        <thead
+                                class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
+                        >
+                        <tr>
+                            <th>
+                            </th>
+                            <th>
+                            </th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr
+                                class="bg-white border-b "
+                        >
+                            <td class="px-6 py-4">
+                                작성자
+                            </td>
+                            <td class="px-6 py-4">
+                                사원명
+                            </td>
+                        </tr>
+
+                        <tr
+                                class="bg-white border-b "
+                        >
+                            <td class="px-6 py-4">
+                                항목
+                            </td>
+                            <td class="px-6 py-4">
+                                <dropdown placement="left">
+                                    <template v-slot:button>
+                                        <button class="flex px-5 py-3 rounded-md bg-blue-100 text-gray-400">
+                                            선택
+                                            <span class="ml-5 mt-1"
+                                            ><Icon icon="ant-design:caret-down-filled"
+                                            /></span>
+                                        </button>
+                                    </template>
+                                    <template v-slot:content>
+                                        <div class="">
+                                            <a
+                                                    href="#"
+                                                    class="block hover:bg-blue-100 hover:text-gray-400 px-4 py-2"
+                                            >Action 1</a
+                                            >
+                                            <a
+                                                    href="#"
+                                                    class="block mt-4 hover:bg-blue-100 hover:text-gray-400 px-4 py-2"
+                                            >Action 2</a
+                                            >
+                                            <a
+                                                    href="#"
+                                                    class="block mt-4 hover:bg-blue-100 hover:text-gray-400 px-4 py-2"
+                                            >Action 3</a
+                                            >
+                                            <a
+                                                    href="#"
+                                                    class="block mt-4 hover:bg-blue-100 hover:text-gray-400 px-4 py-2"
+                                            >Action 4</a
+                                            >
+                                        </div>
+                                    </template>
+                                </dropdown>
+                            </td>
+                        </tr>
+                        <tr
+                                class="bg-white border-b "
+                        >
+                            <td class="px-6 py-4">
+                                항목분류
+                            </td>
+                            <td class="px-6 py-4">
+                                <dropdown placement="left">
+                                    <template v-slot:button>
+                                        <button class="flex px-5 py-3 rounded-md bg-blue-100 text-gray-400">
+                                            선택
+                                            <span class="ml-5 mt-1"
+                                            ><Icon icon="ant-design:caret-down-filled"
+                                            /></span>
+                                        </button>
+                                    </template>
+                                    <template v-slot:content>
+                                        <div class="">
+                                            <a
+                                                    href="#"
+                                                    class="block hover:bg-blue-100 hover:text-gray-400 px-4 py-2"
+                                            >Action 1</a
+                                            >
+                                            <a
+                                                    href="#"
+                                                    class="block mt-4 hover:bg-blue-100 hover:text-gray-400 px-4 py-2"
+                                            >Action 2</a
+                                            >
+                                            <a
+                                                    href="#"
+                                                    class="block mt-4 hover:bg-blue-100 hover:text-gray-400 px-4 py-2"
+                                            >Action 3</a
+                                            >
+                                            <a
+                                                    href="#"
+                                                    class="block mt-4 hover:bg-blue-100 hover:text-gray-400 px-4 py-2"
+                                            >Action 4</a
+                                            >
+                                        </div>
+                                    </template>
+                                </dropdown>
+                            </td>
+                        </tr>
+                        <tr
+                                class="bg-white border-b "
+                        >
+                            <td class="px-6 py-4">
+                                제목
+                            </td>
+                            <td class="px-6 py-4">
+                                <input
+                                        type="text"
+                                        placeholder=""
+                                        class="p-3 w-full bg-blue-100 rounded-md outline-none"
+                                />
+                            </td>
+                        </tr>
+                        <tr class="bg-white border-b">
+                            <td class="px-6 py-4">
+                                일시
+                            </td>
+                            <td class="px-6 py-4 flex flex-wrap">
+                                <div class="w-60 flex">
+                                    <input class="p-3 bg-blue-100 rounded-md outline-none" type="datetime-local"/>
+                                </div>
+                                <p>_</p>
+                                <div class="px-6 w-80 flex">
+                                    <input class="p-3 bg-blue-100 rounded-md outline-none" type="datetime-local"/>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr
+                                class="bg-white border-b "
+                        >
+                            <td class="px-6 py-4">
+                                내용입력
+                            </td>
+                            <td class="px-6 py-4">
+                                <textarea
+                                        type="text"
+                                        style="white-space: pre-line;"
+                                        placeholder=""
+                                        class="p-3 w-full h-20 bg-blue-100 rounded-md outline-none"
+                                />
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                    <br>
+                    <div class="flex ">
+                        <p class="px-6 text-sm dark:text-gray-400">
+                            1차 결재자
+                            <input
+                                    type="text"
+                                    placeholder=""
+                                    class="p-3 w-full bg-gray-100 rounded-md outline-none"
+                            />
+                        </p>
+                        <p class="px-6 text-sm dark:text-gray-400">
+                            2차 결재자
+                            <input
+                                    type="text"
+                                    placeholder=""
+                                    class="p-3 w-full bg-gray-100 rounded-md outline-none"
+                            />
+                        </p>
+                        <button
+                                type="button"
+                                class="text-white bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 border dark:border-gray-700-lg border dark:border-gray-700-red-500/50 dark:border dark:border-gray-700-lg dark:border dark:border-gray-700-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+                        >
+                            결재자 선택
+                        </button>
+                    </div>
+                    <div class="button-container">
+                        <button
+                                type="button"
+                                class="text-white bg-gradient-to-r from-yellow-400 via-yellow-500 to-yellow-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-yellow-300 dark:focus:ring-yellow-800 border dark:border-gray-700-lg border dark:border-gray-700-red-500/50 dark:border dark:border-gray-700-lg dark:border dark:border-gray-700-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+                        >
+                            신청
+                        </button>
+                        <button
+                                type="button"
+                                class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 border dark:border-gray-700-lg border dark:border-gray-700-red-500/50 dark:border dark:border-gray-700-lg dark:border dark:border-gray-700-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+                        >
+                            취소
+                        </button>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+</template>
+
+<script>
+import { Icon } from "@iconify/vue";
+import Dropdown from "@/components/Dropdown.vue";
+export default {
+    components: {
+        Dropdown,
+        Icon,
+    },
+    methods: {
+        alert() {
+            alert("Hello");
+        },
+    },
+};
+</script>
+<style>
+.button-container {
+    display: flex;
+    justify-content: flex-end;
+}
+</style>
---
+++ src/views/ea/eaRequestList.vue
@@ -0,0 +1,186 @@
+<template>
+    <div class="dashboard p-4">
+
+        <div class="mt-2 lg:flex block lg:gap-2 relative">
+            <div
+                    class="mt-2 bg-white dark:bg-gray-800 p-5 w-full rounded-md box-border border dark:border-gray-700"
+            >
+                <h2 class="font-bold text-lg text-gray-800 dark:text-gray-200">
+                    전자결재 신청내역
+                </h2>
+                <div class="wrapper-button flex justify-between mt-3">
+                    <p
+                            class="dark:bg-gray-800 dark:hover:bg-gray-700 border dark:border-gray-700 max-w-lg px-4 py-3 block rounded-md text-gray-500 dark:text-gray-400"
+                    >
+                        현재날짜
+                    </p>
+                    <div
+                            class="input-box border dark:bg-gray-900 lg:ml-0 ml-5 dark:border-gray-700 rounded-md hidden lg:w-search w-full box-border lg:flex md:flex focus-within:bg-gray-100 dark:focus-within:bg-gray-700"
+                    >
+                      <span class="text-3xl p-2 text-gray-400"
+                      ><Icon icon="ei:search"
+                      /></span>
+                        <input
+                                type="text"
+                                placeholder="Search..."
+                                class="p-3 w-full bg-white dark:bg-gray-900 dark:text-gray-400 rounded-md outline-none focus:bg-gray-100 dark:focus:bg-gray-700"
+                        />
+                    </div>
+                </div>
+                <div class="wrapping-table mt-10">
+                    <table
+                            class="w-full text-sm text-left text-gray-500 dark:text-gray-400 lg:overflow-auto overflow-x-scroll"
+                    >
+                        <thead
+                                class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
+                        >
+                        <tr>
+                            <th
+                                    scope="col"
+                                    class="uppercase px-6 py-3"
+                            >
+                                번호
+                            </th>
+                            <th
+                                    scope="col"
+                                    class="uppercase px-6 py-3"
+                            >
+                                항목
+                            </th>
+                            <th
+                                    scope="col"
+                                    class="uppercase px-6 py-3"
+                            >
+                                항목구분
+                            </th>
+                            <th
+                                    scope="col"
+                                    class="uppercase px-6 py-3"
+                            >
+                                제목
+                            </th>
+                            <th
+                                    scope="col"
+                                    class="uppercase px-6 py-3"
+                            >
+                                신청일시
+                            </th>
+                            <th
+                                    scope="col"
+                                    class="uppercase px-6 py-3"
+                            >
+                                사용일시
+                            </th>
+                            <th
+                                    scope="col"
+                                    class="uppercase px-6 py-3"
+                            >
+                                상태
+                            </th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr
+                                class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 odd:bg-white even:bg-gray-50"
+                                v-for="items in tableTransaction"
+                                :key="items.transaction"
+                        >
+                            <td class="px-6 py-4">
+                                {{ items.transaction }}
+                            </td>
+                            <td class="px-6 py-4">
+                                {{ items.item }}
+                            </td>
+                            <td class="px-6 py-4">
+                                {{ items.sub_item }}
+                            </td>
+                            <td class="px-6 py-4">
+                                {{ items.title }}
+                            </td>
+                            <td class="px-6 py-4">
+                                {{ items.datetime1 }}
+                            </td>
+                            <td class="px-6 py-4">
+                                {{ items.datetime2 }}
+                            </td>
+                            <td class="px-6 py-4">
+                                {{ items.status }}
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import { Icon } from "@iconify/vue";
+
+export default {
+
+    name: "Dashboard",
+    data() {
+        return {
+            tableTransaction: [
+                {
+                    transaction: "1",
+                    item: "휴가",
+                    sub_item: "연차",
+                    title: "연차 사용",
+                    datetime1: "2019.04.09",
+                    datetime2: "2019.04.10 ~ 2019.04.12",
+                    status: "완료",
+                },
+                {
+                    transaction: "1",
+                    item: "휴가",
+                    sub_item: "포상휴가",
+                    title: "포상휴가 사용",
+                    datetime1: "2019.04.19",
+                    datetime2: "2019.04.20 ~ 2019.04.22",
+                    status: "대기",
+                },
+                {
+                    transaction: "1",
+                    item: "휴가",
+                    sub_item: "공가",
+                    title: "공가 사용",
+                    datetime1: "2019.04.09",
+                    datetime2: "2019.04.10 ~ 2019.04.12",
+                    status: "반려",
+                },
+                {
+                    transaction: "1",
+                    item: "휴가",
+                    sub_item: "연차",
+                    title: "연차 사용",
+                    datetime1: "2019.04.09",
+                    datetime2: "2019.04.10 ~ 2019.04.12",
+                    status: "대기",
+                },
+                {
+                    transaction: "1",
+                    item: "차량",
+                    sub_item: "차량A 신청",
+                    title: "회사 차량 신청",
+                    datetime1: "2019.04.10",
+                    datetime2: "2019.04.10 ~ 2019.04.10",
+                    status: "완료",
+                },
+
+            ],
+        };
+        // end chart data line
+    },
+    components: {
+        Icon,
+    },
+    mounted() {},
+};
+</script>
