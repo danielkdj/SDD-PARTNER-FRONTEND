57ea080bef1fc3479132c384d7b1576ed43febc9
danielkdj <danieldj8585@gmail.com>
2023-05-15 10:41:53 +0900

    update several errors

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,380 @@
+<template>
+  <!-- sidebar -->
+    <nav class="sidebar bg-white dark:bg-gray-800">
+        <!-- sidebar head -->
+        <div class="sidebar-head p-4">
+            <router-link
+                    to="/"
+                    exact
+                    class="flex"
+            >
+                <img
+                        class="w-8 mt-1"
+                        src="@/assets/logo.png"
+                        alt="logo SSD"
+                />
+                <h2
+                        class="text-2xl font-normal ml-3 mt-2 text-gray-800 dark:text-gray-200"
+                        translate="no"
+                >
+                    SDD
+                </h2>
+            </router-link>
+            <div
+                    class="bg-gray-700 absolute mt-3 dark:block hidden rounded-md py-1 px-2 text-xs text-gray-200"
+            >
+                Dark mode
+            </div>
+            <button
+                    class="lg:hidden block dark:text-gray-400 float-right -mt-7"
+                    @click="$emit('sidebarToggle')"
+            >
+                <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        aria-hidden="true"
+                        role="img"
+                        width="25px"
+                        height="25px"
+                        preserveAspectRatio="xMidYMid meet"
+                        viewBox="0 0 32 32"
+                >
+                    <path
+                            fill="currentColor"
+                            d="M7.219 5.781L5.78 7.22L14.563 16L5.78 24.781l1.44 1.439L16 17.437l8.781 8.782l1.438-1.438L17.437 16l8.782-8.781L24.78 5.78L16 14.563z"
+                    />
+                </svg>
+            </button>
+        </div>
+        <!-- sidebar list -->
+        <div class="sidebar-list p-4 mt-4 divide-y dark:divide-gray-700">
+            <div class="pb-5">
+                <p class="font-medium text-gray-400 dark:text-gray-400">Menu</p>
+                <div class="wrap-item mt-4 dark:text-gray-500">
+                    <div class="item">
+
+                        <menu-accordion>
+
+                            <template v-slot:icon>
+
+                                <Icon icon="bxs:dashboard"/>
+
+                            </template>
+
+                            <template v-slot:title> My Page</template>
+
+                            <template v-slot:content>
+
+                                <router-link
+
+                                        to="/mypage/myInfo"
+
+                                        class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700"
+
+                                >
+
+                                    내 정보 보기
+
+                                </router-link>
+
+                                <router-link
+
+                                        to="/mypage/changePassword"
+
+                                        class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700"
+
+                                >
+
+                                    비밀번호 변경
+
+                                </router-link>
+
+                                <router-link
+
+                                        to="/mypage/appointmentInfo"
+
+                                        class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700"
+
+                                >
+
+                                    발령정보 조회
+
+                                </router-link>
+
+                                <router-link
+
+                                        to="/mypage/requestCounsel"
+
+                                        class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700"
+
+                                >
+
+                                    상담신청
+
+                                </router-link>
+
+                            </template>
+
+                        </menu-accordion>
+
+                    </div>
+                    <div class="item mt-3">
+
+                        <menu-accordion>
+
+                            <template v-slot:icon>
+
+                                <Icon icon="bxs:dashboard"/>
+
+                            </template>
+
+                            <template v-slot:title> 전자결재</template>
+
+                            <template v-slot:content>
+
+                                <router-link
+
+                                        to="/ea/eaRequest"
+
+                                        class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700"
+
+                                >
+
+                                    전자결재 신청
+
+                                </router-link>
+
+                                <router-link
+
+                                        to="/ea/eaRequestList"
+
+                                        class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700"
+
+                                >
+
+                                    전자결재 신청내역 조회
+
+                                </router-link>
+
+                                <router-link
+
+                                        to="/ea/eaApprovalList"
+
+                                        class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700"
+
+                                >
+
+                                    전자결재 요청내역 조회
+
+                                </router-link>
+
+                            </template>
+
+                        </menu-accordion>
+
+                    </div>
+
+                    <!-- 오늘은 요기까지 -->
+                    <div class="item mt-3">
+                        <menu-accordion>
+                            <template v-slot:icon>
+                                <Icon icon="bxs:dashboard"/>
+                            </template>
+                            <template v-slot:title> 근태관리</template>
+                            <template v-slot:content>
+                                <router-link
+                                        to="/component/alert"
+                                        class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700"
+                                >
+                                    근태 내역 조회
+                                </router-link>
+                                <router-link
+                                        to="/component/accordion"
+                                        class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700"
+                                >
+                                    휴가 사용현황 조회
+                                </router-link>
+
+                            </template>
+                        </menu-accordion>
+                    </div>
+                    <div class="item mt-3">
+                        <menu-accordion>
+                            <template v-slot:icon>
+                                <Icon icon="bxs:dashboard"/>
+                            </template>
+                            <template v-slot:title> 일정관리</template>
+                            <template v-slot:content>
+                                <router-link
+                                        to="/component/alert"
+                                        class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700"
+                                >
+                                    회의실 사용일정 조회
+                                </router-link>
+                                <router-link
+                                        to="/component/accordion"
+                                        class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700"
+                                >
+                                    차량 사용일정 조회
+                                </router-link>
+                                <router-link
+                                        to="/component/badge"
+                                        class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700"
+                                >
+                                    팀 일정 조회
+                                </router-link>
+
+                            </template>
+                        </menu-accordion>
+                    </div>
+                    <div class="item mt-3">
+                        <div class="item">
+                            <router-link
+                                    to="/component/badge"
+                                    exact
+                                    class="w-full flex text-left rounded-md box-border p-3 hover:bg-gray-200 dark:hover:bg-gray-700"
+                            >
+                                <span class="mr-3 text-xl"><Icon icon="bxs:dashboard"/></span>
+                                <span class="w-full"> 공지사항 </span>
+                            </router-link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="pb-5">
+                <p class="font-medium text-gray-400 mt-4 dark:text-gray-400">
+                    관리부서 Menu
+                </p>
+
+                <div class="item mt-3">
+                    <menu-accordion>
+                        <template v-slot:icon>
+                            <Icon icon="gg:components"/>
+                        </template>
+                        <template v-slot:title> 근태 & 근태</template>
+                        <template v-slot:content>
+                            <router-link
+                                    to="/component/alert"
+                                    class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700"
+                            >
+                                급여대장 등록
+                            </router-link>
+                            <router-link
+                                    to="/component/accordion"
+                                    class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700"
+                            >
+                                급여목록 조회
+                            </router-link>
+                            <router-link
+                                    to="/component/badge"
+                                    class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700"
+                            >
+                                근태등록 조회
+                            </router-link>
+                            <router-link
+                                    to="/component/breadcumb"
+                                    class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700"
+                            >
+                                휴가등록 조회
+                            </router-link>
+
+                        </template>
+                    </menu-accordion>
+                </div>
+
+                <div class="item mt-3">
+                    <menu-accordion>
+                        <template v-slot:icon>
+                            <Icon icon="gg:components"/>
+                        </template>
+                        <template v-slot:title> 인사</template>
+                        <template v-slot:content>
+                            <router-link to="/HR/employee"
+                                         class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700">
+                                사원관리
+                            </router-link>
+
+                            <router-link to="/HR/counseling"
+                                         class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700">
+                                상담
+                            </router-link>
+
+                            <router-link to="/HR/AD"
+                                         class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700">
+                                입사서류
+                            </router-link>
+
+                            <router-link to="/HR/insurance"
+                                         class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700">
+                                4대보험
+                            </router-link>
+
+                            <router-link to="/HR/dispatch"
+                                         class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700">
+                                발령
+                            </router-link>
+                        </template>
+                    </menu-accordion>
+                </div>
+
+                <div class="item mt-3">
+                    <menu-accordion>
+                        <template v-slot:icon>
+                            <Icon icon="gg:components"/>
+                        </template>
+                        <template v-slot:title> 총무</template>
+                        <template v-slot:content>
+                            <router-link
+                                    to="/component/badge"
+                                    class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700"
+                            >
+                                공지사항
+                            </router-link>
+                            <router-link
+                                    to="/component/breadcumb"
+                                    class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700"
+                            >
+                                비품구매
+                            </router-link>
+                            <router-link
+                                    to="/component/button"
+                                    class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700"
+                            >
+                                회의실 사용
+                            </router-link>
+                            <router-link
+                                    to="/component/card"
+                                    class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700"
+                            >
+                                차량사용
+                            </router-link>
+                            <router-link
+                                    to="/component/dropdown"
+                                    class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700"
+                            >
+                                법정 의무교육
+                            </router-link>
+                            <router-link
+                                    to="/component/modal"
+                                    class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700"
+                            >
+                                서류함
+                            </router-link>
+                        </template>
+                    </menu-accordion>
+                </div>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+import {Icon} from "@iconify/vue";
+import MenuAccordion from "./MenuAccordion.vue";
+
+export default {
+    components: {
+        Icon,
+        MenuAccordion,
+    },
+};
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,344 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+// Default Pages
+import Dashboard from "../views/Dashboard.vue";
+// Component Pages
+import Valert from "../views/components/alert.vue";
+import Vaccrodion from "../views/components/accordion.vue";
+import Vbadges from "../views/components/badges.vue";
+import Vbreadcumb from "../views/components/breadcumbs.vue";
+import Vbutton from "../views/components/button.vue";
+import Vcard from "../views/components/card.vue";
+import Vdropdown from "../views/components/dropdown.vue";
+import Vmodal from "../views/components/modal.vue";
+import Login from "../views/layouts/auth/Login.vue";
+import Register from "../views/layouts/auth/Register.vue";
+import ForgotPassword from "../views/layouts/auth/forgot-password.vue";
+
+// layouts
+import Blank from "../views/layouts/Blank.vue";
+
+// error page
+import Page404 from "../views/layouts/error/404.vue";
+import Page500 from "../views/layouts/error/500.vue";
+import PageMaintenance from "../views/layouts/error/maintenance.vue";
+import Tables from "../views/tables.vue";
+
+
+
+
+//Mypage Pages  (김영우 5월4일 추가)
+import MyInfo from "@/views/mypage/myInfo.vue";
+import ChangePassword from "@/views/mypage/changePassword.vue";
+import AppointmentInfo from "@/views/mypage/appointmentInfo.vue";
+import RequestCounsel from "@/views/mypage/requestCounsel.vue";
+
+//EA(전자결재) Pages  (김영우 5월9일 추가)
+import EaRequest from "@/views/ea/eaRequest.vue";
+import EaRequestList from "@/views/ea/eaRequestList.vue";
+import EaApprovalList from "@/views/ea/eaApprovalList.vue";
+import AdminCreate from "@/views/admin/AdminCreate.vue";
+import TeamCalendarWrite from "@/views/teamCalendar/TeamCalendarWrite.vue";
+import AdminPermission from "@/views/admin/AdminPermission.vue";
+import DocumentList from "@/views/document/DocumentList.vue";
+import DocumentDetail from "@/views/document/DocumentDetail.vue";
+import TeamCalendar from "@/views/teamCalendar/TeamCalendar.vue";
+import DocumentWrite from "@/views/document/DocumentWrite.vue";
+import attendance from "@/views/attendance/attendance.vue";
+import searchemplyoee from "@/views/attendance/searchemplyoee.vue";
+import Salary from "@/views/salary/salary.vue";
+import dayoff from "@/views/dayoff/dayoff.vue";
+
+
+var appname = " - Windzo Dashboard Admin Template";
+
+const routes = [
+  // Routes
+  {
+    path: "/",
+    name: "Dashboard",
+    component: Dashboard,
+    meta: { title: "Dashboard " + appname },
+  },
+
+  // Components based Routes
+  {
+    path: "/component/alert",
+    name: "Valert",
+    component: Valert,
+    meta: { title: "Alert" + appname },
+  },
+  {
+    path: "/component/accordion",
+    name: "Vaccordion",
+    component: Vaccrodion,
+    meta: { title: "Accordion" + appname },
+  },
+  {
+    path: "/component/badge",
+    name: "Vbadge",
+    component: Vbadges,
+    meta: { title: "Badge" + appname },
+  },
+  {
+    path: "/component/breadcumb",
+    name: "Vbreadcumb",
+    component: Vbreadcumb,
+    meta: { title: "Breadcumb" + appname },
+  },
+  {
+    path: "/component/button",
+    name: "Vbutton",
+    component: Vbutton,
+    meta: { title: "Button" + appname },
+  },
+  {
+    path: "/component/card",
+    name: "Vcard",
+    component: Vcard,
+    meta: { title: "Card" + appname },
+  },
+  {
+    path: "/component/dropdown",
+    name: "Vdropdown",
+    component: Vdropdown,
+    meta: { title: "Dropdown" + appname },
+  },
+  {
+    path: "/component/modal",
+    name: "Vmodal",
+    component: Vmodal,
+    meta: { title: "Modal" + appname },
+  },
+  // layouts
+
+  {
+    path: "/Blank",
+    name: "Blank Page",
+    component: Blank,
+    meta: { title: "Blank Page" + appname },
+  },
+
+  {
+    path: "/auth/login",
+    name: "Login",
+    component: Login,
+    meta: { title: "Login" + appname, hideNav: true },
+  },
+  {
+    path: "/auth/register",
+    name: "Register",
+    component: Register,
+    meta: { title: "Register" + appname, hideNav: true },
+  },
+  {
+    path: "/auth/forgot-password",
+    name: "ForgotPassword",
+    component: ForgotPassword,
+    meta: { title: "i Forgot Password" + appname, hideNav: true },
+  },
+  // layout/error
+  // default page 404
+  {
+    path: "/:pathMatch(.*)*",
+    name: "Page404",
+    component: Page404,
+    meta: { title: "Upps! 404" + appname, hideNav: true },
+  },
+  {
+    path: "/500",
+    name: "Page500",
+    component: Page500,
+    meta: { title: "Server internal Error" + appname, hideNav: true },
+  },
+  {
+    path: "/maintenance",
+    name: "maintenance",
+    component: PageMaintenance,
+    meta: {
+      title: "Sorry the app has been Maintenance" + appname,
+      hideNav: true,
+    },
+  },
+  {
+    path: "/tables",
+    name: "Tables",
+    component: Tables,
+    meta: {
+      title: "Tables" + appname,
+    },
+  },
+  {
+    path: "/HR/employee",
+    name: "Employee",
+    component: () => import("../views/HR/Employee.vue"),
+    meta: {
+        title: "Employee" + appname,
+    }
+  },
+  {
+    path: "/HR/counseling",
+    name: "Counseling",
+    component: () => import("../views/HR/Counseling.vue"),
+    meta: {
+        title: "Counseling" + appname,
+    }
+  },
+  {
+    path: "/HR/AD",
+    name: "AD",
+    component: () => import("../views/HR/AD.vue"),
+    meta: {
+        title: "AD" + appname,
+    }
+  },
+  {
+    path: "/HR/insurance",
+    name: "Insurance",
+    component: () => import("../views/HR/Insurance.vue"),
+    meta: {
+        title: "Insurance" + appname,
+    }
+  },
+  {
+    path: "/HR/dispatch",
+    name: "Dispatch",
+    component: () => import("../views/HR/Dispatch.vue"),
+    meta: {
+        title: "Dispatch" + appname,
+    }
+  },
+        //mypage (김영우 5/4일 추가)
+  {
+    path: "/mypage/myInfo",
+    name: "MyInfo",
+    component: MyInfo,
+    meta: {
+      title: "MyInfo" + appname,
+    },
+  },
+  {
+    path: "/mypage/changePassword",
+    name: "ChangePassword",
+    component: ChangePassword,
+    meta: {
+      title: "ChangePassword" + appname,
+    },
+  },
+  {
+    path: "/mypage/appointmentInfo",
+    name: "AppointmentInfo",
+    component: AppointmentInfo,
+    meta: {
+      title: "AppointmentInfo" + appname,
+    },
+  },
+  {
+    path: "/mypage/requestCounsel",
+    name: "RequestCounsel",
+    component: RequestCounsel,
+    meta: {
+      title: "RequestCounsel" + appname,
+    },
+  },
+    // 전자결재 항목 (김영우 5/9일 추가)
+  {
+    path: "/ea/eaRequest",
+    name: "EaRequest",
+    component: EaRequest,
+    meta: {
+      title: "EaRequest" + appname,
+    },
+  },
+  {
+    path: "/ea/eaRequestList",
+    name: "EaRequestList",
+    component: EaRequestList,
+    meta: {
+      title: "EaRequestList" + appname,
+    },
+  },
+  {
+    path: "/ea/eaApprovalList",
+    name: "EaApprovalList",
+    component: EaApprovalList,
+    meta: {
+      title: "EaApprovalList" + appname,
+    },
+  },
+  {
+    path: "/dayoff",
+    name: "dayoff",
+    component: dayoff,
+
+
+  },
+  {
+    path: "/salary",
+    name: "Salary",
+    component: Salary,
+
+
+  },
+  {
+    path: "/attendance",
+    name: "attendance",
+    component: attendance,
+
+  },
+  {
+    path: "/searchemplyoee",
+    name: "searchemplyoee",
+    component: searchemplyoee,
+
+  },
+  {
+    path: "/document",
+    name: "document",
+    component: DocumentList
+  },
+  {
+    path: "/documentDetail/:id",
+    name: "documentDetail",
+    component: DocumentDetail
+  },
+  {
+    path: "/teamCalendar",
+    name: "teamCalendar",
+    component: TeamCalendar
+  },
+  {
+    path: "/documentWrite",
+    name: "documentWrite",
+    component: DocumentWrite
+  },
+  {
+    path: "/adminPermission",
+    name: "adminPermission",
+    component: AdminPermission
+  },
+  {
+    path: "/teamCalendarWrite",
+    name: "teamCalendarWrite",
+    component: TeamCalendarWrite
+  },
+  {
+    path: "/adminCreate",
+    name: "adminCreate",
+    component: AdminCreate
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+//
+  linkExactActiveClass: "exact-active",
+});
+
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title;
+  next();
+});
+
+export default router;
