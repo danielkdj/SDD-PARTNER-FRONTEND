d8a3183541f534236e61ff9b41976d6e69812674
chou <asha.jisu@gmail.com>
2023-05-22 14:05:58 +0900

    Notice REFACTOR

--- src/views/affair/NoticeList.vue
+++ src/views/affair/NoticeList.vue
@@ -103,17 +103,6 @@ export default {
             },
             requestBody:{},
             tableTransaction  : ref([]),
-            // tableTransaction: [
-            //     {
-            //         noticeNo: 1,
-            //         title: '제목',
-            //         createdAt: '작성일시',
-            //         users : {
-            //             userName: '작성자',
-            //         }
-            //
-            //     },
-            // ],
         }
     },
     watch: {
@@ -153,25 +142,9 @@ export default {
                 query: this.requestBody
             })
         },
-        // fnSearch(){
-        //     this.requestBody = {
-        //         title: this.sTitle
-        //     }
-        //     this.$axios.get(this.$serverUrl + "/notice/list",{
-        //         params: this. requestBody,
-        //         headers: {}
-        //     }).then((res)=>{
-        //         this.list = res.data.data
-        //     }).catch((err) => {
-        //         if (err.message.indexOf('Network Error') > -1) {
-        //             alert('네트워크가 원활하지 않습니다.\n잠시 후 다시 시도해주세요.')
-        //         }
-        //     })
-        // }
     },
     mounted() { //페이지로드시 함수 적용
         this.fnGetList()
     }
-
 }
 </script>
